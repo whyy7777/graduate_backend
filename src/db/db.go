fd55b9a7fda8e5c0e91d7b173131d225486db377
whyy7777 <yinyhuang7@gmail.com>
2023-04-17 12:33:20 +0800

    debug password

--- src/db/db.go
+++ src/db/db.go
@@ -11,6 +11,7 @@ import (
 var db *sql.DB
 
 func InitDB(password string) (err error) {
+	fmt.Println(password)
 	dsn := "root:" + password + "@tcp(1.117.65.130:3306)/music_online"
 	db, err = sql.Open("mysql", dsn)
 	if err != nil {
