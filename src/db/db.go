2d7235de7618b13219391d1b271b1d1be0c09c3a
whyy7777 <yinyhuang7@gmail.com>
2023-04-25 22:13:46 +0800

    get songs of a album

--- src/db/db.go
+++ src/db/db.go
@@ -229,6 +229,21 @@ func GetAlbums(singer string) []common.Album {
 
 func GetAlbumSongs(albumId string) []common.Song {
 	songs := make([]common.Song, 0)
-
+	sqlStr := `SELECT songId FROM album_songs WHERE albumId = '` + albumId + `';`
+	songIds, err := db.Query(sqlStr)
+	if err != nil {
+		return nil
+	}
+	for songIds.Next() {
+		var temp common.Song
+		var id string
+		songIds.Scan(&id)
+		fmt.Println(id)
+		sqlStr = `SELECT * FROM songs WHERE id = ` + id + `;`
+		fmt.Println(sqlStr)
+		song := db.QueryRow(sqlStr)
+		song.Scan(&temp.Id, &temp.SongName, &temp.Singer, &temp.ReleaseDate)
+		songs = append(songs, temp)
+	}
 	return songs
 }
