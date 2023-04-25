4beee5029520673a00d1c294ed9036cc01998099
whyy7777 <yinyhuang7@gmail.com>
2023-04-25 21:58:30 +0800

    get all albums

--- src/db/db.go
+++ src/db/db.go
@@ -211,3 +211,18 @@ func GetHotPlaylists(userId string) []common.Playlist {
 	}
 	return data
 }
+
+func GetAlbums(singer string) []common.Album {
+	data := make([]common.Album, 0)
+	sqlStr := `SELECT * FROM albums WHERE singer = '` + singer + `';`
+	albums, err := db.Query(sqlStr)
+	if err != nil {
+		return nil
+	}
+	for albums.Next() {
+		var temp common.Album
+		albums.Scan(&temp.AlbumId, &temp.AlbumName, &temp.Singer, &temp.ReleaseData)
+		data = append(data, temp)
+	}
+	return data
+}
