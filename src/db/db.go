d5222c6769cb9bb34d1dabbfba492f372b9fc7a1
whyy7777 <yinyhuang7@gmail.com>
2023-05-05 22:49:59 +0800

    hide databse password

--- src/db/db.go
+++ src/db/db.go
@@ -2,13 +2,11 @@ package db
 
 import (
 	"database/sql"
-	"fmt"
 )
 
 var db *sql.DB
 
 func InitDB(password string) (err error) {
-	fmt.Println(password)
 	dsn := "root:" + password + "@tcp(1.117.65.130:3306)/music_online"
 	db, err = sql.Open("mysql", dsn)
 	if err != nil {
